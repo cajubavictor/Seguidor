b90eb6512cb89ff469e706c603b70411e71df971
Kelly Bezerra da Mota <kelly.bezerra.mota@gmail.com>
2017-05-22 20:55:20 -0300

    Add files via upload

---
+++ TESTANDOMOTORES_22.05/TESTANDOMOTORES_22.05.ino
@@ -0,0 +1,19 @@
+//sololearn
+
+#define MOTOR_E1    10
+#define MOTOR_E2    9
+#define MOTOR_D1    6
+#define MOTOR_D2    5
+void setup(){
+  pinMode(MOTOR_E1, OUTPUT);
+  pinMode(MOTOR_E2, OUTPUT);
+  pinMode(MOTOR_D1, OUTPUT);
+  pinMode(MOTOR_D2, OUTPUT);
+}
+void loop(){
+  analogWrite(MOTOR_E1, 200);
+  digitalWrite(MOTOR_E2, LOW);
+  digitalWrite(MOTOR_D1, LOW);
+  analogWrite(MOTOR_D2, 200);
+}
+
